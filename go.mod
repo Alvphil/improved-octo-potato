4174f31a296b1d4e6bde702e2ddf3fd52f8a5af5
Alvphil <philip.nyblom@gmail.com>
2023-12-25 20:16:58 +0100

    Added api routing

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Alvphil/improved-octo-potato.git
 
 go 1.21.4
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.0.11 // indirect
+)
--- main.go
+++ main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	r := chi.NewRouter()
+	apiCfg := apiConfig{0}
+	r.Mount("/api/", api(&apiCfg))
 
-	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	r.Handle("/app", fsHandler)
+	r.Handle("/app/*", fsHandler)
+	//r.Get("/healthz", handlerReadiness)
+	//r.Get("/metrics", apiCfg.handlerMetricsInc)
+	//r.HandleFunc("/reset", apiCfg.handlerResetMetrics)
 
-	corsMux := middlewareCors(mux)
+	corsMux := middlewareCors(r)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -28,4 +41,40 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerMetricsInc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, cfg.numberServerHits())
+}
+
+func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits = 0
+}
+
+func (cfg apiConfig) numberServerHits() string {
+	return ("Hits: " + fmt.Sprint(cfg.fileserverHits))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++ // Increment the counter here
+		log.Printf("number of hits: %v", cfg.fileserverHits)
+		next.ServeHTTP(w, r) // Call the next handler
+	})
+
+	//cfg.fileserverHits++
+	//log.Printf("number of hits: %v", cfg.fileserverHits)
+	//return next
+}
+
+func api(apiCfg *apiConfig) http.Handler {
+	r := chi.NewRouter()
+	r.Get("/healthz", handlerReadiness)
+	r.Get("/metrics", apiCfg.handlerMetricsInc)
+	r.HandleFunc("/reset", apiCfg.handlerResetMetrics)
+	return r
 }
