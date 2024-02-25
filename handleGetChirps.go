0836c656b3945ea1bebfb67d683c518544bbffeb
Alvphil <philip.nyblom@gmail.com>
2024-02-25 19:57:07 +0100

    added sorting param, and author param

--- handleGetChirps.go
+++ handleGetChirps.go
@@ -16,17 +16,46 @@ func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	chirps := []database.Chirp{}
-	for _, dbChirp := range dbChirps {
-		chirps = append(chirps, database.Chirp{
-			Author_id: dbChirp.Author_id,
-			Body:      dbChirp.Body,
-			ID:        dbChirp.ID,
-		})
+
+	s := r.URL.Query().Get("author_id")
+	// s is a string that contains the value of the author_id query parameter
+	// if it exists, or an empty string if it doesn't
+
+	if s != "" {
+		s, err := strconv.Atoi(s)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "invalid author ID")
+			return
+		}
+		for _, dbChirp := range dbChirps {
+			if dbChirp.Author_id == s {
+				chirps = append(chirps, database.Chirp{
+					Author_id: dbChirp.Author_id,
+					Body:      dbChirp.Body,
+					ID:        dbChirp.ID,
+				})
+			}
+		}
+	} else {
+		for _, dbChirp := range dbChirps {
+			chirps = append(chirps, database.Chirp{
+				Author_id: dbChirp.Author_id,
+				Body:      dbChirp.Body,
+				ID:        dbChirp.ID,
+			})
+		}
 	}
 
-	sort.Slice(chirps, func(i, j int) bool {
-		return chirps[i].ID < chirps[j].ID
-	})
+	sortedParam := r.URL.Query().Get("sort")
+	if sortedParam == "desc" {
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].ID > chirps[j].ID
+		})
+	} else {
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].ID < chirps[j].ID
+		})
+	}
 
 	respondWithJSON(w, http.StatusOK, chirps)
 }
